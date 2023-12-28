cdd301cf80170acb74a178aa73a2bd3305c79142
Ulyana <119431787+MatveevaUlyana@users.noreply.github.com>
2023-12-28 21:02:06 +0000

    Финальный файл

--- DZ2.py
+++ DZ2.py
@@ -6,11 +6,11 @@ df = pd.read_csv('/workspaces/Introduction_Python_MGIMO/Mall_Customers.csv')
 
 average1 = df['Spending Score (1-100)'].mean()
 most_freq = df['Genre'].value_counts().idxmax()
-average3 = df['Age'].mean()
+average2 = df['Age'].mean()
 
 print("Средние траты клиента ", average1)
 print("Пол клиента ", most_freq)
-print("Возраст клиента ", average3)
+print("Возраст клиента ", average2)
 
 
 df['Gender'] = df['Genre'].map({'Male': 0, 'Female': 1})
@@ -20,4 +20,15 @@ cluster_centers = pd.DataFrame(kmeans.cluster_centers_, columns=df.columns)
 rich_clients = cluster_centers.loc[cluster_centers['Annual Income (k$)'].idxmax()]
 
 labels = kmeans.labels_
-rich_clients_df = df.loc[labels == rich_clients_label]
+rich_clients_df = df.loc[labels == rich_clients.name]
+
+rich_clients_df['Gender'] = rich_clients_df['Gender'].map({0: 'Male', 1: 'Female'})
+rich_clients_df
+
+average1 = rich_clients_df['Spending Score (1-100)'].mean()
+most_freq = rich_clients_df['Gender'].value_counts().idxmax()
+average2 = rich_clients_df['Age'].mean()
+
+print("Средние траты богатого клиента ", average1)
+print("Пол богатого клиента ", most_freq)
+print("Возраст богатого клиента ", average2)
